63f6aad4417dd00e6d01a931abfac133243899ea
saloev <saloev.saadi@yandex.ru>
2020-02-02 01:24:14 +0300

    feat: basic home page

---
+++ src/components/home/Intro.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home__intro">
+        <h1 class="home__intro-tile color--primary margin--0">Saloev Sadi</h1>
+        <p class="home__intro-subtitle color--white">Frontend Developer</p>
+    </div>
+</template>
+
+<style lang="scss">
+    .home__intro {
+        &-title {
+        }
+
+        &-subtitle {
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="home">
-    test
+  <div class="home height-min--100vh">
+    <intro></intro>
   </div>
 </template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+import Intro from '@/components/home/Intro.vue';
+
+@Component({
+  components: {
+    Intro,
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss">
+.home {
+  background-position: center;
+  background-size: cover;
+  background-image: url($assets+"/home-bg.jpeg"),
+    linear-gradient(
+      180deg,
+      rgba(8, 15, 26, 0.59) 0%,
+      rgba(17, 17, 46, 0.46) 100%
+    );
+  background-blend-mode: multiply;
+}
+</style>
